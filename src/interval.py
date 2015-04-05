1ed898e4a498bcaf27906ca2e24370ea920e2324
Guido <me@guido.io>
2015-04-05 17:25:59 +0200

    Cleanup interval generation

--- src/interval.py
+++ src/interval.py
@@ -1,6 +1,4 @@
 from __future__ import division
-import terplot
-import pylab
 
 def drange(start, stop, step):
      r = start
@@ -8,33 +6,23 @@ def drange(start, stop, step):
      	yield r
      	r += step
 
-resolution = 16
+
+def gen_intervals(resolution = 16):
+    
+    
+    pos = [x for x in drange(1/resolution, (resolution-1)/resolution, 1/resolution)]
+    
+    intervals = []
+    err = 0.00001
+    
+    for x1 in pos:
+        for x2 in pos:
+            for x3 in pos:
+                if x1 + x2 + x3 > 1.0 - err and x1 + x2 + x3 < 1.0 + err:
+                    intervals.append( [x1,x2,x3])
+
+    return intervals
 
 
-pos = [x for x in drange(1/resolution, (resolution-1)/resolution, 1/resolution)]
-
-intervals = []
-err = 0.00001
-
-for x1 in pos:
-    for x2 in pos:
-        for x3 in pos:
-            if x1 + x2 + x3 > 1.0 - err and x1 + x2 + x3 < 1.0 + err:
-                intervals.append( [x1,x2,x3])
-
-
-#print intervals
-print len(intervals)
-data = pylab.vstack(intervals)
-#terplot.ternaryPlot(data)
-s=1000
-
-newdata,ax = terplot.ternaryPlot(data)
-
-ax.scatter(
-    newdata[:,0],
-    newdata[:,1],
-    s=64,
-    alpha=0.8,
-    )
-pylab.show()
+if __name__ == '__main__':
+    print gen_intervals(8)
