5cb9b4378da6cf550bb9c1c9b9b47bd55240338c
Guido <me@guido.io>
2015-04-06 16:26:57 +0200

    Minimal interval option in generation

--- src/interval.py
+++ src/interval.py
@@ -1,5 +1,6 @@
 from __future__ import division
 
+#Multidimensional range
 def drange(start, stop, step):
      r = start
      while r < stop:
@@ -7,7 +8,7 @@ def drange(start, stop, step):
      	r += step
 
 
-def gen_intervals(resolution = 16):
+def gen_intervals(resolution = 16, minimal_interval=0):
     
     
     pos = [x for x in drange(1/resolution, (resolution-1)/resolution, 1/resolution)]
@@ -18,7 +19,11 @@ def gen_intervals(resolution = 16):
     for x1 in pos:
         for x2 in pos:
             for x3 in pos:
+                
+                # Has a total length of 1 second
                 if x1 + x2 + x3 > 1.0 - err and x1 + x2 + x3 < 1.0 + err:
-                    intervals.append( [x1,x2,x3])
+                   # Doesn't have very short intervals
+                   if x1 > minimal_interval and x2 > minimal_interval and x3 > minimal_interval:
+                        intervals.append( [x1,x2,x3])
 
     return intervals
