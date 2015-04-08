6fa6242c5c37dd95b04d9d25ea8f0496c26ba467
Guido <me@guido.io>
2015-04-08 22:28:13 +0200

    Remove unnormalized fractions code

--- src/ternplot/fractionserror.m
+++
@@ -1,9 +0,0 @@
-% FRACTIONS normalise ternary data
-%   [fA, fB, fC] = FRACTIONS(A, B, C) calculates fractional values for 
-
-function [fA, fB, fC] = fractionserror(A, B, C, D, E, F)
-Total = (A+B+C);
-fA = D./Total;
-fB = E./Total;
-fC = F./Total;
-
