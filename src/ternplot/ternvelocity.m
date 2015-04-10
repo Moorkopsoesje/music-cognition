e0376b31105632f03d377d5f53a42e44940ee027
Guido <me@guido.io>
2015-04-10 04:02:27 +0200

    Cleanup ternvelocity

--- src/ternplot/ternvelocity.m
+++ src/ternplot/ternvelocity.m
@@ -12,21 +12,8 @@ end;
 [fD, fE, fF] = fractions(D,E,F);
 [v, w] = terncoords(fD, fE, fF);
 
-% Sort data points in x order
-%[x, i] = sort(x);
-%y = y(i);
-
-% Sort data points in v order
-%[v, i] = sort(v);
-%w = w(i);
-
-
-
 % Make ternary axes
 [hold_state, cax, next] = ternaxes(majors);
-
-%q = scatter(x,y, 'filled', 'k');
-%q = scatter(v,w, 'r');
 
 % plot data quiveroni
     q = quiver(x, y, v-x, w-y, varargin{:}, 'Autoscale','off');
