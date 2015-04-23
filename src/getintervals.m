9d88d46a81b445ccd1c15d8c78abf686ee73f676
Guido <me@guido.io>
2015-04-23 22:30:59 +0200

    Write intervals to file

--- src/getintervals.m
+++ src/getintervals.m
@@ -1,9 +1,11 @@
-midi = readmidi('e01.mid')
+subject = 'e01';
+
+midi = readmidi(strcat('data/',subject,'.mid'))
 
 Notes=midiInfo(midi,0);
 whos Notes
 
-last_index = 328
+last_index = size(Notes,1);
 
 c = Notes(1:last_index,5);
 
@@ -15,3 +17,9 @@ while j~=last_index+1
     j=j+1;
     i=i+1;
 end
+
+filename = strcat('intervals/',subject,'.txt');
+
+fileID = fopen(filename, 'w+');
+fprintf(fileID,'%f\n', intervals);
+fclose(fileID);
