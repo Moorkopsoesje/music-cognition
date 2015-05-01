8708ced1f9128ed35c1ca48defe8751dd74aa4e9
inezwijnands <inezwijnands@gmail.com>
2015-05-01 11:06:40 +0200

    Add some comments to code

--- src/averageintervals.m
+++ src/averageintervals.m
@@ -1,5 +1,7 @@
 clear all;
 
+% (First you need to run the files to read in the intervals per participant)
+
 interv = csvread('all_intervals.csv');
 length=size(interv,1);
 
@@ -9,10 +11,12 @@ length=size(interv,1);
 
 part_names = ['e01'; 'e02'; 'e03'; 'e04'; 'e05'; 'e06'; 'o01'; 'o02'; 'o03'; 'o04'; 'o05'; 'o06'];
 
+% Errors
+
 all = 0;
 
 for i = 1:size(part_names,1)
-    
+    % Read recorded intervals per participant
     filename_indices = strcat('intervals/filtered-indices-', part_names(i,:),'.csv');
     filename_intervals = strcat('intervals/filtered-intervals-', part_names(i,:),'.csv');
     
@@ -26,7 +30,7 @@ for i = 1:size(part_names,1)
     else
         all = vertcat(all, ind_intervals);
     end
-    
+    % Calculate errors per participant
     dist_sum = 0;
     for j = 1:size(ind_intervals,1)
         index = ind_intervals(j,1)+1;
@@ -48,7 +52,7 @@ files = all;
 sortedmatrix = files(order,:);
 
 
-
+% Average per participant
  
 j=0;
 i=1;
@@ -56,14 +60,18 @@ startpos=1;
 counter = 1;
 B = zeros(66,4);
 
+% loop over all different original intervals
 for k=1:66
+    % loop over the amount of recorded intervals for the original interval
     while(sortedmatrix(i,1) == j && counter < size(sortedmatrix,1))
         counter = counter +1;
         i = i +1;
     end
-        B(k,:)=mean(sortedmatrix(startpos:counter-1,:));
-        startpos=counter;
-        j=j+1;
+    % average the recorded intervals per participant per original
+    % interval
+    B(k,:)=mean(sortedmatrix(startpos:counter-1,:));
+    startpos=counter;
+    j=j+1;
     
 end
 
@@ -77,30 +85,32 @@ startpos = 1;
 counter = 1;
 entropy = zeros(66,1);
 
+% loop over original intervals
 for k=1:66
-    %afst is nu nul
+    % loop over all recorded intervals for the original interval
     sdist = 0;
     n = 0;
     while(sortedmatrix(i,1) == j && counter < size(sortedmatrix,1))
         counter = counter +1;
-        %afst += afstand tot mean
+        % calculate distance of recorded interval to average (entropy) 
+        % of intervals of participant
         from = sortedmatrix(i,2:4);
         to = B(k,2:4);
         sdist = sdist + dist(from, to');
-        % ik heb er n
+        % count how many intervals per participant
         n = n + 1;
         i = i +1;
     end
-        startpos=counter;
-        j=j+1;
+    startpos=counter;
+    j=j+1;
         
-        %afst / n
-        entropy(k) = sdist/n;
+    % Normalize over how many intervals per participant
+    entropy(k) = sdist/n;
         
-        n = 0;
+    n = 0;
     
 end
 
 csvwrite('intervals/entropy.csv',entropy);
-entropy;
+%entropy;
     
